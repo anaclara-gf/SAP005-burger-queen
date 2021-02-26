053ab3d55e09cf83623bfa572ac5314a5acd96b4
anaclara-gf <anaclaragf@outlook.com>
2021-02-26 18:05:53 -0300

    rotas privadas funcionarios cozinha

--- src/pages/Entregue.js
+++ src/pages/Entregue.js
@@ -2,15 +2,33 @@ import '../style/pagina404.css'
 import React from "react";
 import Footer from "../components/Footer";
 import Header from '../components/Header';
+import Logo from "../components/Logo";
+
+const role = localStorage.getItem("role");
+
 
 function Entregue() {
     return (
         <>
-          <Header />
+        {role === "cozinha" ? (
+          <>
+            <Header />
             <main >
               <p>Pedidos Entregues</p>
             </main>
             <Footer />
+          </>
+        ) : (
+          <>
+            <Header />
+            <main className="acessonegado-container">
+                <p className="acessonegado-title">Ops!!!</p>
+                <p className="acessonegado-message">Você não pode acessar essa página!</p>
+                <Logo />
+            </main>
+            <Footer />
+          </>
+            )}
         </>
     );
 }
